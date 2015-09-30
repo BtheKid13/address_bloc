b09801d41d74a4d7eb4d9b469b747b2073d9739c
Wil Burke <billyburke13@gmail.com>
2015-09-29 23:15:53 -0400

    added rspec test file

--- spec/address_book.rb
+++ spec/address_book.rb
@@ -1,4 +1,4 @@
-require_relative '../models/address_book'
+require_relative "../models/address_book"
 
 RSpec.describe AddressBook do
   describe "attributes" do
@@ -17,23 +17,37 @@ RSpec.describe AddressBook do
       expect(book.entries.size).to eq(0)
     end
   end
+    describe "#add_entry" do
+      it "adds an entry to the address book" do
+        book = AddressBook.new
+        book.add_entry("Wil Burke", "813.282.3332", "hello@gmail.com")
+        expect(book.entries.size).to eq(1)
+      end
+      it "adds the correct information to entries" do
+        book = AddressBook.new
+        book.add_entry("Wil Burke", "813.282.3332", "hello@gmail.com")
+        new_entry = book.entries[0]
 
-  describe "#add_entry" do
-    it "adds only one entry to the address book" do
-      book = AddressBook.new
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-
-      expect(book.entries.size).to eq(1)
+        expect(new_entry.name).to eq("Wil Burke")
+        expect(new_entry.phone_number).to eq("813.282.3332")
+        expect(new_entry.email).to eq("hello@gmail.com")
     end
+  end
+    describe "#remove_entry" do
+      it "removes an entry using the address book info" do
+        book = AddressBook.new
+        book.add_entry("Jessie James", "813.431.0278", "jj@gmail.com")
 
-    it "adds the correct information to entries" do
-      book = AddressBook.new
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-      new_entry = book.entries[0]
+        name = "Wil Burke"
+        phone_number = "813.282.3332"
+        email = "hello@gmail.com"
+        book.add_entry(name, phone_number, email)
 
-      expect(new_entry.name).to eq('Ada Lovelace')
-      expect(new_entry.phone_number).to eq('010.012.1815')
-      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+        expect(book.entries.size).to eq 2
+        book.remove_entry(name, phone_number, email)
+        expect(book.entries.size).to eq 1
+        expect(book.entries.first.name).to eq("Jessie James")
+
     end
   end
 end
