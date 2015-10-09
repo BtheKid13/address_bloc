6582c167145572d9b30ed2d2fc7743f819f06733
Wil Burke <billyburke13@gmail.com>
2015-10-09 18:52:38 -0400

    Adding Assignment files

--- models/address_book.rb
+++ models/address_book.rb
@@ -38,6 +38,40 @@ class AddressBook
       row_hash = row.to_hash # <-converts each item in csv into a symbol to be used in a hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
 
+  def iterative_search(name)
+    @entries.each do |entry|
+
+      if entry.name == name
+        return entry
+      else
+        return nil
+      end
+    end
+  end
+
+
+  def binary_search(name)
+    #1
+    lower = 0
+    upper = entries.length - 1
+
+    #2
+    while lower <= upper
+    #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+    #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+    return nil #5
   end
 end
